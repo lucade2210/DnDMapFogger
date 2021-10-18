d6d630f1655dbe20b87fe832996d9a74661e0619
Luca van der Knaap <lucade2210@gmail.com>
2021-10-18 14:21:48 +0200

    V1.0 Working with polygon

--- Functions.py
+++ Functions.py
@@ -4,29 +4,27 @@ import numpy as np
 import random
 
 def drawLine(image, x, y, coordinateList):
-    cv2.line(image, (x, y), coordinateList[-1], (255, 255, 0), 1)
+    cv2.line(image, (x, y), coordinateList[-1], (255,0,0), 2)
 
 def drawText(image, x, y):
-    cv2.putText(image, 'Start', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 1, cv2.LINE_AA)
+    cv2.putText(image, 'Start', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2, cv2.LINE_AA)
 
-def great_circle_distance(coords1, coords2):
-    d = pi / 180
-    answer =  acos(sin(coords1[1]*d) * sin(coords2[1]*d) +
-              cos(coords1[1]*d) * cos(coords2[1]*d) *
-              cos((coords1[0] - coords2[0]) * d)) / d
-    return answer
-
-def checkCoordinatesListInRange(x, y, coordinateList, range):
+def checkCoordinatesListInRange(x, y, coordinateList, clickRange):
     for coords in coordinateList:
-        if great_circle_distance((x, y), coords) < range:
+        if coords[0] - clickRange <= x <= coords[0] + clickRange and coords[1] - clickRange <= y <= coords[1] + clickRange:
             return True
     return False
             
-def drawShape(imgShaped, coordinateList):
+def drawShape(img, coordinateList):
     pts =  [np.array([list(elem) for elem in coordinateList], np.int32)]
-    B = random.randint(0, 255)
-    G = random.randint(0, 255)
-    R = random.randint(0, 255)
-    cv2.fillPoly(imgShaped, pts, (B, G, R))
+    cv2.fillPoly(img, pts, (0,0,0))
+
+def overlayMask(mask, img, imgOG, color):
+    img = np.where(mask == 0, imgOG, color)
+    return img
+
+def overlayMaskTransparent(mask, img, imgOG):
+    img = np.where(mask != 0, imgOG, img)
+    return img
     
 
--- Main.py
+++ Main.py
@@ -1,54 +1,60 @@
 import cv2
 import Functions as f
+import numpy as np
 
-range = 8
-scale = 0.25
+clickRange = 8
+scaleSize = 0.25
 shapeList = []
 coordinateList = []
+textColor = 255
+maskColor = 100
 
 def click_event(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        global img
-        global imgMasked
-        global coordinateList
-        global shapeList
+        global img, imgDark, fogMask, drawMask, imgViewer, imgDrawer, imgDrawerTemp, coordinateList, shapeList
 
         if len(coordinateList) >= 1:
-            f.drawLine(mask, x, y, coordinateList)
+            f.drawLine(imgDrawerTemp, x, y, coordinateList)
         else:
-            f.drawText(mask, x, y)
+            f.drawText(imgDrawerTemp, x, y)
         
-        if f.checkCoordinatesListInRange(x, y, coordinateList, range):
-            f.drawShape(mask, coordinateList)
-            imgMasked = cv2.addWeighted(img, 0.4, mask, 0,1, 0)
-            cv2.imshow('image', imgMasked)
+        if f.checkCoordinatesListInRange(x, y, coordinateList, clickRange):
+            f.drawShape(fogMask, coordinateList)
+            drawMask = np.zeros((dimY, dimX, 3), np.uint8)
+            imgViewer = f.overlayMask(fogMask, imgViewer, img, maskColor)
+            imgDrawer = f.overlayMaskTransparent(fogMask, img, imgDark)
+            imgDrawerTemp = imgDrawer.copy()
+            cv2.imshow('viewer', imgViewer)
+            cv2.imshow('drawer', imgDrawer)
             shapeList.append(coordinateList[:])
             coordinateList.clear()
-            print('Shapes: ' + str(shapeList))
-            print('')
 
         else:
-            cv2.imshow('image', imgMasked)
+            cv2.imshow('drawer', imgDrawerTemp)
             coordinateList.append((x, y))
-            print('')
 
+if __name__ == '__main__':
+    imgOriginal = cv2.imread('Map Cragmaw Cave.jpg')
+    maskOriginal = cv2.imread('Mask.png')
+    dimX = int(imgOriginal.shape[1] * scaleSize)
+    dimY = int(imgOriginal.shape[0] * scaleSize)
+    dim = (dimX, dimY)
 
+    img = cv2.resize(imgOriginal, dim, interpolation = cv2.INTER_AREA)
+    imgDark = img.copy()
+    imgDark = cv2.add(imgDark, np.array([-40.0]))
+    
+    fogMask = np.full((dimY, dimX, 3), maskColor, np.uint8)
+    drawMask = np.zeros((dimY, dimX, 3), np.uint8)
+    imgViewer = img.copy()
+    imgDrawer = img.copy()
+    imgViewer = f.overlayMask(fogMask, imgViewer, img, maskColor)
+    imgDrawer = np.where(fogMask == 0, imgDrawer, imgDark)
+    imgDrawerTemp = imgDrawer.copy()
 
-imgOriginal = cv2.imread('Map Cragmaw Cave.jpg')
-maskOriginal = cv2.imread('TransparentMask.png')
+    cv2.imshow('viewer', imgViewer)
+    cv2.imshow('drawer', imgDrawer)
+    cv2.setMouseCallback('drawer', click_event)
 
-dim = (int(imgOriginal.shape[1] * scale), int(imgOriginal.shape[0] * scale))
-
-img = cv2.resize(imgOriginal, dim, interpolation = cv2.INTER_AREA)
-mask = cv2.resize(maskOriginal, dim, interpolation = cv2.INTER_AREA)
-imgMasked = cv2.addWeighted(img, 1, mask, 1, 0)
-
-
-
-cv2.imshow('image', imgMasked)
-
-cv2.setMouseCallback('image', click_event)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
