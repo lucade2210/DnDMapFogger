df4e1f8998227cbe1765a081d89a4c78b44a53b2
Luca van der Knaap <lucade2210@gmail.com>
2021-10-25 20:34:03 +0200

    V2.0 Working with fog and drawing

--- Functions.py
+++ Functions.py
@@ -1,7 +1,6 @@
 import cv2
 from math import *
 import numpy as np
-import random
 
 def drawLine(image, x, y, coordinateList):
     cv2.line(image, (x, y), coordinateList[-1], (255,0,0), 2)
@@ -9,22 +8,18 @@ def drawLine(image, x, y, coordinateList):
 def drawText(image, x, y):
     cv2.putText(image, 'Start', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2, cv2.LINE_AA)
 
-def checkCoordinatesListInRange(x, y, coordinateList, clickRange):
-    for coords in coordinateList:
-        if coords[0] - clickRange <= x <= coords[0] + clickRange and coords[1] - clickRange <= y <= coords[1] + clickRange:
-            return True
-    return False
-            
+def drawDot(image, x, y, radius):
+    cv2.circle(image, (x, y), radius, (255,0,0), 2)
+    
 def drawShape(img, coordinateList):
     pts =  [np.array([list(elem) for elem in coordinateList], np.int32)]
-    cv2.fillPoly(img, pts, (0,0,0))
+    cv2.fillPoly(img, pts, (1,1,1))
 
-def overlayMask(mask, img, imgOG, color):
-    img = np.where(mask == 0, imgOG, color)
-    return img
-
-def overlayMaskTransparent(mask, img, imgOG):
-    img = np.where(mask != 0, imgOG, img)
-    return img
-    
-
+def checkCoordinatesWithStart(x, y, coordinateList, clickRange):
+    coords = coordinateList[0]
+    if coords[0] - clickRange <= x <= coords[0] + clickRange and coords[1] - clickRange <= y <= coords[1] + clickRange:
+        return True
+    return False
+            
+def overlayMask(mask, imgOnZero, imgOnOne):
+    return np.where(mask, imgOnOne, imgOnZero)
--- Main.py
+++ Main.py
@@ -2,68 +2,101 @@ import glob
 import cv2
 import Functions as f
 import numpy as np
+import random
+import os
 
-clickRange = 8
-scaleSize = 0.25
+#Base variables and array declaration
+clickRange = 10
+imageScale = 1
 shapeList = []
 coordinateList = []
-textColor = 255
-maskColor = 100
 
+#Main Function for the on mouse click event
+#Will check and execute what to do on click in the current situation
 def click_event(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        global img, imgDark, fogMask, drawMask, imgViewer, imgDrawer, imgDrawerTemp, coordinateList, shapeList
+        global imgBase, imgBaseDark, fogMask, imgFog, imgDrawn, imgViewer, imgDrawer, coordinateList, shapeList
 
+        #If coordinatelist has values, than we already have a starting point
+        #So start drawing lines or end the shape if the start position is click again
         if len(coordinateList) >= 1:
-            f.drawLine(imgDrawerTemp, x, y, coordinateList)
-        else:
-            f.drawText(imgDrawerTemp, x, y)
-        
-        if f.checkCoordinatesListInRange(x, y, coordinateList, clickRange):
-            f.drawShape(fogMask, coordinateList)
-            drawMask = np.zeros((dimY, dimX, 3), np.uint8)
-            imgViewer = f.overlayMask(fogMask, imgViewer, img, maskColor)
-            imgDrawer = f.overlayMaskTransparent(fogMask, img, imgDark)
-            imgDrawerTemp = imgDrawer.copy()
-            cv2.imshow('viewer', imgViewer)
-            cv2.imshow('drawer', imgDrawer)
-            shapeList.append(coordinateList[:])
-            coordinateList.clear()
+            f.drawLine(imgDrawn, x, y, coordinateList)
 
+            #Check if the click coordinates are the same as the start position
+            # If so, then we draw the shape in the fogmask and overlay this mask on both the viewer and drawer layers
+            if f.checkCoordinatesWithStart(x, y, coordinateList, clickRange):
+                f.drawShape(fogMask, coordinateList)
+                imgViewer = f.overlayMask(fogMask, imgFog, imgBase)
+                imgDrawer = f.overlayMask(fogMask, imgBaseDark, imgBase)
+                imgDrawn = imgDrawer.copy()
+                cv2.imshow('viewer', imgViewer)
+                cv2.imshow('drawer', imgDrawer)
+                shapeList.append(coordinateList[:])
+                coordinateList.clear()
+
+            #If not clicked on starting position, then just simply show the newly drawn line on the drawer layer
+            else:
+                cv2.imshow('drawer', imgDrawn)
+                coordinateList.append((x, y))
+
+        #This else is reached when it is the first click of a new shape, making the starting position
         else:
-            cv2.imshow('drawer', imgDrawerTemp)
+            f.drawText(imgDrawn, x, y)
+            f.drawDot(imgDrawn, x, y, clickRange)
+            cv2.imshow('drawer', imgDrawn)
             coordinateList.append((x, y))
-
+        
 if __name__ == '__main__':
+
+    #Open all files in the Maps folder and let the user choose one
     mapNames = []
     fileCounter = 0
     for file in glob.glob("Maps\*"):
         mapNames.append(file)
         print(str(fileCounter) + ": " + str(file)[5:])
+        fileCounter = fileCounter + 1
     print("")
     chosenMap = int(input("Choose your map by number + enter: "))
 
-
+    #ImgOriginal = The unedited original image
+    #DimX & DimY are dimensions of the image in pixels
     imgOriginal = cv2.imread(mapNames[chosenMap])
-    dimX = int(imgOriginal.shape[1] * scaleSize)
-    dimY = int(imgOriginal.shape[0] * scaleSize)
+    dimX = int(imgOriginal.shape[1] * imageScale)
+    dimY = int(imgOriginal.shape[0] * imageScale)
     dim = (dimX, dimY)
 
-    img = cv2.resize(imgOriginal, dim, interpolation = cv2.INTER_AREA)
-    imgDark = img.copy()
-    imgDark = cv2.add(imgDark, np.array([-40.0]))
-    
-    fogMask = np.full((dimY, dimX, 3), maskColor, np.uint8)
-    drawMask = np.zeros((dimY, dimX, 3), np.uint8)
-    imgViewer = img.copy()
-    imgDrawer = img.copy()
-    imgViewer = f.overlayMask(fogMask, imgViewer, img, maskColor)
-    imgDrawer = np.where(fogMask == 0, imgDrawer, imgDark)
-    imgDrawerTemp = imgDrawer.copy()
+    #ImgBase will be the unedited fallback image when revealing parts of the map
+    #ImgBaseDark is the darkend version for the drawer window
+    imgBase = cv2.resize(imgOriginal, dim, interpolation = cv2.INTER_AREA)
+    imgBaseDark = cv2.add(imgBase.copy(), np.array([-40.0]))
 
+    #FogMask will remember the drawn shapes in a boolean array
+    #imgFog will be the fog image that is actually used to overlay the base image
+    fogMask = np.zeros((dimY, dimX, 3), np.uint8)
+    randomFile = "Fogs\\" + random.choice(os.listdir("Fogs\\"))
+    imgFog = cv2.resize(cv2.imread(randomFile), dim, interpolation = cv2.INTER_AREA)
+
+    #imgViewer is the compiled image to show in the viewer window
+    #imgdrawer is the compiled image to show in the drawer window
+    #imgDrawn is the temporarily compiled image to show in the drawer window when drawing. Only this layer will show the lines
+    imgViewer = imgFog.copy()
+    imgDrawer = imgBaseDark.copy()
+    imgDrawn = imgDrawer.copy()
+
+    #This is how they will be compiled in the f.overlayMask function
+    #Viewer -> imgFog (mask=0) : imgBase (mask=1)
+    #Drawer -> imgBaseDark (mask=0) : imgBase (mask=1)
+
+
+    cv2.namedWindow('viewer', cv2.WINDOW_FREERATIO)
+    cv2.namedWindow('drawer', cv2.WINDOW_FREERATIO)
+    cv2.moveWindow('viewer',1000,500)
+    cv2.moveWindow('drawer',-1920 ,0)
+    cv2.setWindowProperty('viewer', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.imshow('viewer', imgViewer)
     cv2.imshow('drawer', imgDrawer)
     cv2.setMouseCallback('drawer', click_event)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
