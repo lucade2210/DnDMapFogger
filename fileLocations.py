d759db5110fed469c19033957b76acea0434807c
LVDKnaap <lucade2210@gmail.com>
2022-05-07 16:30:04 +0200

    filelocation

---
+++ fileLocations.py
@@ -0,0 +1,4 @@
+class fileLocations:
+    locationDesktop = 'G:\My Drive\D&D\Waterdeep Dragon Heist\Session #11\Maps\*'
+    locationLaptop = 'G:\\My Drive\\D&D\\Waterdeep Dragon Heist\\Session #11\\Maps\\*'
+    
